ff7216451aac1fd6900621a66f7188098a82dcc1
Max <bazarsadtsogtjargal@gmail.com>
2022-02-11 23:24:57 +0900

    add property page

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>{{ home.title }}</div>
+  <div style="display-flex;">
+    <div>
+      <img
+        v-for="image in home.images"
+        :key="image"
+        alt="something descriptive"
+        :src="image"
+        width="200"
+        height="150"
+      />
+    </div>
+    {{ home.title }}
+    ${{ home.pricePerNight }} / night <br />
+    {{ home.location.address }} {{ home.location.city }}
+    <img src="/images/marker.svg" width="20" height="20" />
+    {{ home.location.state }}
+    {{ home.location.country }}
+    <br />
+    <img src="/images/star.svg" width="20" height="20" />
+    {{ home.reviewValue }} <br />
+    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds,
+    {{ home.bathrooms }} bath <br />
+  </div>
 </template>
 
 <script>
 import homes from '~/data/homes';
 export default {
+  head() {
+    return {
+      title: this.home.title,
+    };
+  },
   data() {
     return {
       home: {},
