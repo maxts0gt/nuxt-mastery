f97b5c3a22e870809f928108a347c91da59e7dd8
Max <bazarsadtsogtjargal@gmail.com>
2022-02-11 22:49:38 +0900

    create home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>Hello world!!!</div>
+  <div>
+    <div
+      v-for="home in homes"
+      :key="home.objectID"
+      style="float: left; margin: 10px"
+    >
+      <home-card :home="home" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import homes from '~/data/homes';
+
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
